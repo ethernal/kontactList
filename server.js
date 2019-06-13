80496133c564f8f935ec7cac40289a9dbdb1e9ec
Nosa Paath <adepaath@gmail.com>
2019-06-12 19:08:11 -0700

    back end configured

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 // entry point to our backend -> express server
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+
+// connect database
+connectDB();
+
+// Init middleware
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) =>
   res.json({ msg: "Welcome to the ContactList API..." })
